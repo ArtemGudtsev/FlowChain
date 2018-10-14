3d485b7f1bd90f33afb71b8089e34425ea695cb0
Gudtsev Artem <Artem.Gudtsev@gmail.com>
2018-10-14 21:49:35 +0200

    add log assembly

---
+++ src/FlowChain.Log/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FlowChain.Log
+{
+    public static class Logger
+    {
+        public static void SetMsgMethod(Action<string> msg) => Msg = msg;
+        public static readonly Action HoldOnPlease = () => Msg("Press any key to continue");
+        public static readonly Action<string> SubChainCalling = (subChainTitle)
+            => Msg($"### Calling \"{subChainTitle}\" subchain...");
+        public static readonly Action<string> SubChainCompleted = (subChainTitle)
+            => Msg($"### Subchain \"{subChainTitle}\" completed.");
+        public static readonly Action<string> This = (msg) => Msg(msg);
+
+        private static Action<string> Msg { get; set; }
+    }
+}
